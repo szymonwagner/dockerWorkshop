b9e0eded8b70368ea984794662fd6553819e293a
Szymon Wagner <szymon.wagner@intive.com>
2018-03-22 14:25:45 +0100

    testing branch hook

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 let counter = 0
-app.get('/', (req, res) => res.send('Hello World! Number of visit is ' + ++counter))
+app.get('/', (req, res) => res.send(`Hello Hello. Number of visits is ${counter++}`))
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
